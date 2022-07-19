ea0f825581f51b7ee3cfebc0035299971931b47d
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-07-19 06:51:07 -0400

    Implement basic calculator functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate = (operator, a, b) => {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    default:
+      break;
+  }
+};
