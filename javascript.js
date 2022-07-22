4812f0d356b1e604ba5e74239057432c10f3e0c2
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-07-22 06:51:16 -0400

    Make the calculator work, only 2 operands and 1 operator, clear and reset after equals

--- javascript.js
+++ javascript.js
@@ -5,26 +5,107 @@ const divide = (a, b) => a / b;
 
 const operate = (operator, a, b) => {
   switch (operator) {
-    case '+':
+    case 'plus':
       return a + b;
-    case '-':
+    case 'minus':
       return a - b;
-    case '*':
+    case 'times':
       return a * b;
-    case '/':
+    case 'divide':
       return a / b;
     default:
       break;
   }
 };
 
-let key = '';
-
+const display = document.querySelector('.display');
+display.textContent = null;
 const btns = document.querySelectorAll('.btn');
+let key = '';
+let operator = null;
+let operand1 = null;
+let operand2 = null;
+let startOperand = false;
+
+const init = () => {
+  operator = null;
+  operand1 = null;
+  operand2 = null;
+  startOperand = false;
+}
+
+const clear = () => {
+  init();
+  display.textContent = null;
+}
 
 btns.forEach((btn) =>
   btn.addEventListener('click', (e) => {
     key = e.target.textContent;
-    console.log({key});
+
+    if (e.target.classList.contains('clear')) {
+      clear();
+    }
+
+    if (!operand1) {
+      if (e.target.classList.contains('digit')) {
+        if (!startOperand) {
+          display.textContent = null;
+          startOperand = true
+        }
+        display.textContent += key;
+      } else if (e.target.classList.contains('operator')) {
+        setOperand1();
+        startOperand = false;
+        setOperator();
+      }
+    } else if (!operand2) {
+      if (e.target.classList.contains('digit')) {
+        if (!startOperand) {
+          display.textContent = null;
+          startOperand = true
+        }
+        display.textContent += key;
+      } else if (e.target.classList.contains('equals')) {
+        setOperand2();
+        display.textContent = operate(operator, operand1, operand2);
+        init();
+      }
+    }
   })
 );
+
+const setOperand1 = () => {
+  operand1 = Number(display.textContent);
+};
+
+const setOperand2 = () => {
+  operand2 = Number(display.textContent);
+};
+
+const setOperator = () => {
+  if (operand1) {
+    switch (key) {
+      case '\u00F7': // ÷
+        operator = 'divide';
+        console.log(operator);
+        break;
+      case '\u00D7': // ×
+        operator = 'times';
+        console.log(operator);
+        break;
+      case '\u2212': // −
+        operator = 'minus';
+        console.log(operator);
+        break;
+      case '\u002B': // +
+        operator = 'plus';
+        console.log(operator);
+        break;
+      case '\u003D': // =
+        operator = 'equals';
+        console.log(operator);
+        break;
+    }
+  }
+};
